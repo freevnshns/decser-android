a9a8be76b5255b81ff2e4b2c2872bbcf7bf9834e
sidzi <rahulgupta388@gmail.com>
2016-08-28 15:42:39 +0530

    added mumble

--- app/src/main/java/com/ihs/homeconnect/helpers/services.java
+++ app/src/main/java/com/ihs/homeconnect/helpers/services.java
@@ -1,15 +1,18 @@
 package com.ihs.homeconnect.helpers;
 
 public enum services {
-    downloadmanager(6800),
-    backup(80),
-    homebase(80),
-    videosurveillance(8081),
-    xmpp(5222),
-    printer(631);
+    downloadmanager(6800, "DownloadManager"),
+    backup(80, "Backup"),
+    homebase(80, "Dashboard"),
+    videosurveillance(8081, "WebCam"),
+    xmpp(5222, "Chat"),
+    voip(64738, "Audio Call"),
+    printer(631, "Print");
     public final int port;
+    public final String name;
 
-    services(int port) {
+    services(int port, String name) {
         this.port = port;
+        this.name = name;
     }
 }
